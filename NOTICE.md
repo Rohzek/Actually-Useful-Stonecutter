abddcc84fcbe92556fdc8bcc9528ef140f2501c0
Dani S <daniel.sapelli@gmail.com>
2026-05-03 22:19:22 -0400

    Port to NeoForge 1.21.1

---
+++ NOTICE.md
@@ -0,0 +1,20 @@
+# Notice
+
+This repository is an unofficial fork of:
+
+- Rohzek/Actually-Useful-Stonecutter
+- <https://github.com/Rohzek/Actually-Useful-Stonecutter>
+
+Attribution to the original author is preserved.
+
+## Publication Basis
+
+The upstream Modrinth project for Actually Useful Stonecutter is publicly listed under **CC0-1.0**:
+
+- <https://modrinth.com/mod/actually-useful-stonecutter>
+
+This fork relies on that public license declaration for redistribution and modification.
+
+## Scope of This Fork
+
+This fork ports the mod to **NeoForge 1.21.1**. It is not an official continuation and should not be interpreted as endorsed by the original author.
--- src/main/java/com/gmail/rohzek/stonecut/lib/ConfigurationManager.java
+++ src/main/java/com/gmail/rohzek/stonecut/lib/ConfigurationManager.java
@@ -1,32 +1,29 @@
 package com.gmail.rohzek.stonecut.lib;
 
-import net.minecraftforge.common.ForgeConfigSpec;
+import net.neoforged.neoforge.common.ModConfigSpec;
 
-public class ConfigurationManager 
-{
-	private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+public class ConfigurationManager {
+    private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
     public static final General GENERAL = new General(BUILDER);
-    public static final ForgeConfigSpec spec = BUILDER.build();
-    
-    public static class General 
-    {
-    	public final ForgeConfigSpec.ConfigValue<Boolean> isDebug;
-    	public final ForgeConfigSpec.ConfigValue<String> difficulty;
-    	
-    	public General(ForgeConfigSpec.Builder builder) 
-        {
+    public static final ModConfigSpec SPEC = BUILDER.build();
+
+    public static class General {
+        public final ModConfigSpec.ConfigValue<Boolean> isDebug;
+        public final ModConfigSpec.ConfigValue<String> difficulty;
+
+        public General(ModConfigSpec.Builder builder) {
             builder.push("General");
-            
+
             isDebug = builder
                     .comment("Enables/Disables debug mode logging [false/true|default:false]")
                     .translation("debugmode." + Reference.MODID + ".config")
                     .define("isDebug", false);
-            
+
             difficulty = builder
-            		.comment("Determines the difficulty level of the recipes. The easier the difficulty, the more bountiful the results [easy/normal/hard|default:normal]")
-            		.translation("difficulty." + Reference.MODID + ".config")
-            		.define("difficulty", "normal");
-            
+                    .comment("Determines the difficulty level of the recipes. The easier the difficulty, the more bountiful the results [easy/normal/hard|default:normal]")
+                    .translation("difficulty." + Reference.MODID + ".config")
+                    .define("difficulty", "normal");
+
             builder.pop();
         }
     }
